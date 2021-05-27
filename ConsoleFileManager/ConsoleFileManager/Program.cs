4fcfc8053649822a5299deb5395bebb7013eb40b
IVG <vadim207138@gmail.com>
2021-05-28 00:29:59 +0300

    v 0.0.1

--- ConsoleFileManager/ConsoleFileManager/Program.cs
+++ ConsoleFileManager/ConsoleFileManager/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.IO;
 
 namespace ConsoleFileManager
 {
     class Program
     {
         //перечисление возможных команд
+        [Flags]
         enum CommandList
         {
             dir,
@@ -12,36 +14,91 @@ namespace ConsoleFileManager
             copy,
             del,
             info,
-            file
+            file,
+            quit
         };
         static void Main(string[] args)
         {
-            // dir, mkdir, copy, del, info, file
-            //массив возможных команд
-            string[] command = new string[6] {"dir", "mkdir", "copy", "del", "info", "file"};
+            string CurentPath = Directory.GetCurrentDirectory(); // текущая директория (не забыть считать ее из файла конфигурации)
+            string NewCommand = ""; // строка новой команды на выполнение
+            CommandList comm; //рспознанная команда
+
+
+
 
             while (true)
             {
-                Console.WriteLine("Введите команду");
-                string input = Console.ReadLine();
-                if (!ParseCommand(input)) Console.WriteLine("ошибка");
+                // вывод дерева каталогов
+                NewCommand = Console.ReadLine();
+
+                // парсинг, разбор команды
+                if (!ParseCommand(NewCommand)) Console.WriteLine("ошибка");
+                comm = CommandList.copy;
+
+
+                switch (comm)
+                {
+                    case CommandList.dir:
+                        break;
+                    case CommandList.copy:
+                        break;
+                    case CommandList.del:
+                        break;
+                    case CommandList.file:
+                        break;
+                    case CommandList.info:
+                        break;
+                    case CommandList.mkdir:
+                        break;
+                    case CommandList.quit:
+                        break;
+                }
+
+
             }
-            
-            
+
+
             static bool ParseCommand(string CommandString)
             {
-                //разбираем введенную команду на подстроки используя как разделитель знак пробела, исключая дублирование пробелов
+                //разбираем введенную команду на подстроки используя знак пробела как разделитель, исключая дублирование пробелов
                 string[] CommandArray = CommandString.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
                 if (CommandArray.Length > 3) return false; // проверка на количество подстрок, их не может быть болше 3х (команда, аргумент 1, аргумент 2
-                for (int i= 0; i < CommandArray.Length; i++)
+                for (int i = 0; i < CommandArray.Length; i++)
                 {
                     Console.WriteLine(CommandArray[i]);
                 }
                 return true;
-                
             }
-            class command 
+
+            static void DirTree(string Path)
+            {
+
+            }
+
+            static string GetFileInfo(string Path, string FileName) //метод получение информации по файлу
+            {
+                if (File.Exists(Path + FileName)) //проверка существования заданного файла
+                {
+                    string FileDate = File.GetCreationTime(Path + FileName).ToShortDateString(); //дата создания файла
+                    FileAttributes atributes = File.GetAttributes(Path + FileName); //атрибуты файла
+                    int FileSize = (Path + FileName).Length; //размер файла
+
+                    return "  ";
+                }
+                else
+                {
+                    return "False";
+                }
+
+            }
+
+
+            
+        }
+        class FileInfo
+        {
+
         }
     }
 }
